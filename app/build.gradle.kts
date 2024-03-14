8064659346e5c85adcffa13e76ce1f53eb38cf87
mgarbowski <mikolaj.garbowski@gmail.com>
2024-03-14 18:50:25 +0100

    bump postgres driver version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.postgresql:postgresql:42.3.8")
+    implementation("org.postgresql:postgresql:42.7.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
