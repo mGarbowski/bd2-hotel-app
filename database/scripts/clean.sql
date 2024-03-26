31fa16b98ca4fb994ff3ba63997cdf79c5290676
mgarbowski <mikolaj.garbowski@gmail.com>
2024-03-26 14:12:31 +0100

    move sql scripts to separate directory

---
+++ database/scripts/clean.sql
@@ -0,0 +1,20 @@
+BEGIN TRANSACTION;
+-- Drop all tables
+DO $$ DECLARE
+    tabname RECORD;
+BEGIN
+    FOR tabname IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+            EXECUTE 'DROP TABLE IF EXISTS ' || tabname.tablename || ' CASCADE';
+        END LOOP;
+END $$;
+
+-- Drop all sequences
+DO $$ DECLARE
+    seqname RECORD;
+BEGIN
+    FOR seqname IN (SELECT relname FROM pg_class WHERE relkind = 'S' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = current_schema())) LOOP
+            EXECUTE 'DROP SEQUENCE IF EXISTS ' || seqname.relname || ' CASCADE';
+        END LOOP;
+END $$;
+
+COMMIT;
---
+++ database/scripts/query.sql
@@ -0,0 +1,3 @@
+SELECT * FROM country;
+
+SELECT * FROM hotel;
