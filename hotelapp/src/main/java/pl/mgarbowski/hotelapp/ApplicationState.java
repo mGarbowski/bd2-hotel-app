62911962ef17a4d18d0e9fee54a367e9969f639a
mgarbowski <mikolaj.garbowski@gmail.com>
2024-04-01 14:58:00 +0200

    add Customer/User commands and simple login logic

---
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/ApplicationState.java
@@ -0,0 +1,17 @@
+package pl.mgarbowski.hotelapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import pl.mgarbowski.hotelapp.model.Customer;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+@Getter
+@Setter
+public class ApplicationState {
+    private Optional<Customer> user;
+}
