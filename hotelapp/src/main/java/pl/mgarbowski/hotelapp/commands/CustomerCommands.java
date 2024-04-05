6f69d1ac0de23644a1c53224d9a647e1cd3a5fa6
mgarbowski <mikolaj.garbowski@gmail.com>
2024-04-05 11:55:34 +0200

    move Customer business logic to CustomerService

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/CustomerCommands.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/CustomerCommands.java
@@ -2,36 +2,32 @@ package pl.mgarbowski.hotelapp.commands;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.command.annotation.Command;
-import pl.mgarbowski.hotelapp.ApplicationState;
 import pl.mgarbowski.hotelapp.domain.customer.Customer;
-import pl.mgarbowski.hotelapp.domain.customer.CustomerRepository;
+import pl.mgarbowski.hotelapp.domain.customer.CustomerService;
 
 @Command(command = "user")
 @RequiredArgsConstructor
 public class CustomerCommands {
-    private final ApplicationState state;
-    private final CustomerRepository customerRepository;
+    private final CustomerService customerService;
 
     @Command(command = "who", description = "Display currently logged in user")
     public String who() {
-        return state.getUser()
+        return customerService.getLoggedIn()
                 .map(c -> String.format("[%d] %s %s", c.getId(), c.getName(), c.getSurname()))
                 .orElse("You are not currently logged in");
     }
 
     @Command(command = "login", description = "Log in as an existing customer")
     public String login(Integer customerId) {
-        var maybeCustomer = customerRepository.findById(customerId);
-        state.setUser(maybeCustomer);
-        return maybeCustomer
+
+        return customerService.attemptLogin(customerId)
                 .map(c -> String.format("Welcome %s %s!", c.getName(), c.getSurname()))
                 .orElse(String.format("Customer with id [%d] not found", customerId));
     }
 
     @Command(command = "list", description = "List all customers")
     public String list() {
-        var customers = customerRepository
-                .findAll().stream()
+        var customers = customerService.getAll().stream()
                 .map(Customer::toString)
                 .toList();
         return String.join("\n", customers);
---
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/customer/CustomerService.java
@@ -0,0 +1,29 @@
+package pl.mgarbowski.hotelapp.domain.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.mgarbowski.hotelapp.ApplicationState;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final ApplicationState appState;
+
+    public Optional<Customer> getLoggedIn() {
+        return appState.getUser();
+    }
+
+    public Optional<Customer> attemptLogin(Integer customerId) {
+        var maybeCustomer = customerRepository.findById(customerId);
+        appState.setUser(maybeCustomer);
+        return maybeCustomer;
+    }
+
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+}
