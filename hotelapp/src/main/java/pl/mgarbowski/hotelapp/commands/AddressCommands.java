c7191808e06718dfc26332eb6bdefdd9c6124c4d
mgarbowski <mikolaj.garbowski@gmail.com>
2024-04-05 11:48:05 +0200

    move Address business logic to AddressService

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/AddressCommands.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/AddressCommands.java
@@ -3,17 +3,16 @@ package pl.mgarbowski.hotelapp.commands;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.command.annotation.Command;
 import pl.mgarbowski.hotelapp.domain.address.Address;
-import pl.mgarbowski.hotelapp.domain.address.AddressRepository;
+import pl.mgarbowski.hotelapp.domain.address.AddressService;
 
 @RequiredArgsConstructor
 @Command(command = "address")
 public class AddressCommands {
-    private final AddressRepository addressRepository;
+    private final AddressService addressService;
 
     @Command(command = "list")
     public String listAll() {
-        var addresses = addressRepository
-                .findAll().stream()
+        var addresses = addressService.getAll().stream()
                 .map(Address::toString)
                 .toList();
         return String.join("\n", addresses);
---
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/address/AddressService.java
@@ -0,0 +1,15 @@
+package pl.mgarbowski.hotelapp.domain.address;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AddressService {
+    private AddressRepository addressRepository;
+    public List<Address> getAll() {
+        return addressRepository.findAll();
+    }
+}
