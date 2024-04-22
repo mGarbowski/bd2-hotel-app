dcdd8f0b9ce43e1f5b4715f4c19d7b2dd528499e
Michal Luszczek <michluszcz@gmail.com>
2024-04-22 14:30:47 +0200

    add creating update_avg_rating_trigger to schema.sql

--- database/scripts/schema.sql
+++ database/scripts/schema.sql
@@ -159,3 +159,33 @@ CREATE TABLE service_order
 );
 
 COMMIT;
+
+
+CREATE OR REPLACE FUNCTION update_avg_ratings() RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE apartment
+    SET avg_rating = (SELECT avg(rating.star_rating)
+                      FROM rating join public.booking b on rating.booking_id = b.id
+                      where b.apartment_id = apartment.id
+                      GROUP BY b.apartment_id);
+
+    UPDATE hotel
+    SET avg_rating = (SELECT avg(rating.star_rating)
+                      FROM rating join public.booking b on rating.booking_id = b.id
+                                  join public.apartment a on a.id = b.apartment_id
+                      Where hotel.id = a.hotel_id
+                      group by a.hotel_id);
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+-- SELECT update_avg_ratings();
+-- DROP FUNCTION update_avg_ratings();
+
+create or replace trigger update_avg_rating_trigger
+    AFTER INSERT OR UPDATE OR DELETE
+    ON rating
+    for EACH STATEMENT
+execute FUNCTION update_avg_ratings();
+
+
