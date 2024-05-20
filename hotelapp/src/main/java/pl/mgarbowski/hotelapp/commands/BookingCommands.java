f3324f8c4579bc01b9eb811f802031709a3984f8
Aleksander <olek.drwal@gmail.com>
2024-05-20 14:07:18 +0200

    added addding opinions

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/BookingCommands.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/BookingCommands.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.command.annotation.Command;
 import pl.mgarbowski.hotelapp.domain.booking.BookingService;
 import pl.mgarbowski.hotelapp.domain.booking.ComplaintService;
+import pl.mgarbowski.hotelapp.domain.booking.RatingService;
 import pl.mgarbowski.hotelapp.domain.customer.CustomerService;
 
 
@@ -13,6 +14,7 @@ public class BookingCommands {
     private final BookingService bookingService;
     private final ComplaintService complaintService;
     private final CustomerService customerService;
+    private final RatingService ratingService;
 
     @Command(command = "makeComplaint", description = "Make a complaint about a booking")
     public String makeComplaint(Integer bookingId, String complaintText) {
@@ -33,5 +35,26 @@ public class BookingCommands {
             return e.getMessage();
         }
         return "Complaint created";
+    }
+
+    @Command(command = "addOpinion", description = "Add an opinion to a booking")
+    public String addOpinion(Integer bookingId, Integer starRating, String opinionText) {
+        var customer = customerService.getLoggedIn();
+        if(customer.isEmpty()) {
+            return "Not logged in";
+        }
+        var booking = bookingService.getBookingsByCustomer(customer.get())
+                .stream()
+                .filter(b -> b.getId().equals(bookingId))
+                .findFirst();
+        if(booking.isEmpty()) {
+            return "Booking not found";
+        }
+        try {
+            ratingService.createRating(booking.get(), starRating, opinionText);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
+        return "Opinion added";
     }
 }
