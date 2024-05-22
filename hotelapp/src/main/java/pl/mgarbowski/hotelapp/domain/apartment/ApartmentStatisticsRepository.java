48c3e5db2fb58497291ed9e3209d4d7dd23d6dc6
mgarbowski <mikolaj.garbowski@gmail.com>
2024-05-22 14:41:48 +0200

    add CLI command to display statistics for apartment

---
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/apartment/ApartmentStatisticsRepository.java
@@ -0,0 +1,6 @@
+package pl.mgarbowski.hotelapp.domain.apartment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApartmentStatisticsRepository extends JpaRepository<ApartmentStatistics, Integer> {
+}
