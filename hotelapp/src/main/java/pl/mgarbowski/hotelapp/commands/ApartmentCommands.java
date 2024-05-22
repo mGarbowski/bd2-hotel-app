f0aa4a8bceb42f75f8a3470e8329f96615a10ad0
jedrzej-grabski <jedrzej@grabski.pl>
2024-05-22 15:12:24 +0200

    Implemented result formating and result parsing for the searchCity command.

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/ApartmentCommands.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/ApartmentCommands.java
@@ -2,10 +2,12 @@ package pl.mgarbowski.hotelapp.commands;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.command.annotation.Command;
+import pl.mgarbowski.hotelapp.domain.address.Address;
 import pl.mgarbowski.hotelapp.domain.apartment.Apartment;
 import pl.mgarbowski.hotelapp.domain.apartment.ApartmentService;
 import pl.mgarbowski.hotelapp.domain.apartment.InvalidBookingDateException;
 
+import java.security.InvalidParameterException;
 import java.sql.Date;
 import java.util.List;
 
@@ -17,9 +19,18 @@ public class ApartmentCommands {
 
     @Command(command = "searchCity", description = "Search free apartments in city")
     public String searchFreeApartmentsInCity(String city, Date start, Date end) {
+        if (city == null || start == null || end == null) {
+            throw new InvalidParameterException("Parameters cannot be null.");
+        }
         try {
             var apartments = apartmentService.findFreeApartmentsInCity(city, start, end);
-            return formatString(apartments);
+
+            if (apartments.isEmpty()) {
+                return "No apartments available at that date.";
+            } else {
+                return formatString(apartments);
+            }
+
         } catch (InvalidBookingDateException e) {
             return e.getMessage();
         }
@@ -27,8 +38,40 @@ public class ApartmentCommands {
 
     private String formatString(List<Apartment> apartments) {
         return String.join(" ", apartments.stream()
-                .map(Apartment::toString)
+                .map(ApartmentCommands::formatSingleApartment)
                 .toList()
+        );
+    }
+
+    private static String formatAddress(Address address) {
+        return String.format(
+                "%s, %s, %s, %s",
+                address.getStreet(),
+                address.getZipCode(),
+                address.getCity().getName(),
+                address.getCity().getCountry().getName()
+        );
+    }
+
+    private static String formatSingleApartment(Apartment apartment) {
+        return String.format(
+                """
+                            Hotel: [%d] – Apartment number [%d] (%.2f stars) – Capacity: %s – Price per day: %.2f
+                                - Rooms: %d
+                                - Bathrooms: %d
+                                - Beds: %d
+                                - Address: %s \n
+                        """,
+                apartment.getHotel().getId(),
+                apartment.getId(),
+                apartment.getAvg_rating().doubleValue(),
+                apartment.getMaxNPeople() > 1 ?
+                        String.format("%d person", apartment.getMaxNPeople()) : String.format("%d people", apartment.getMaxNPeople()),
+                apartment.getPricePerDay(),
+                apartment.getNRooms(),
+                apartment.getNBathrooms(),
+                apartment.getNBeds(),
+                formatAddress(apartment.getHotel().getAddress())
         );
     }
 }
