bb8b9edaa682ea3976a0c3f16631aceb574e12d9
mgarbowski <mikolaj.garbowski@gmail.com>
2024-05-24 12:03:53 +0200

    display ranking of all hotels

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/StatisticsCommands.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/commands/StatisticsCommands.java
@@ -21,7 +21,7 @@ public class StatisticsCommands {
         return formatApartments(stats);
     }
 
-    @Command(command = "hotel", description = "Show hotel statistics")
+    @Command(command = "hotel", description = "Show statistics for single hotel")
     public String singleHotel(Integer hotelId) {
         var statistics = hotelStatisticsRepository.findById(hotelId).orElseThrow(
                 () -> new IllegalArgumentException("Hotel statistics not found")
@@ -29,11 +29,18 @@ public class StatisticsCommands {
         return formatSingleHotelDetails(statistics);
     }
 
+    @Command(command = "hotels", description = "Show statistics for all hotels")
+    public String allHotels() {
+        var stats = hotelStatisticsRepository.findAll();
+        return formatAllHotels(stats);
+    }
+
     private static String formatApartments(List<ApartmentStatistics> apartments) {
         var rows = apartments.stream()
                 .map(StatisticsCommands::formatSingleApartment)
                 .toList();
-        return "ID\tBookings\tCustomers\tComplaints\tTotal earnings\tAvg earnings\n" + String.join("\n", rows);
+        var header = "ID\tBookings\tCustomers\tComplaints\tTotal earnings\tAvg earnings\n";
+        return header + String.join("\n", rows);
     }
 
     private static String formatSingleApartment(ApartmentStatistics apartment) {
@@ -70,6 +77,27 @@ public class StatisticsCommands {
                 hotelStatistics.getNComplaints(),
                 hotelStatistics.getTotalEarning(),
                 hotelStatistics.getAvgRating()
+        );
+    }
+
+    private static String formatAllHotels(List<HotelStatistics> hotels) {
+        var rows = hotels.stream().map(StatisticsCommands::formatSingleHotelRow).toList();
+        var header = "ID\tAvg Rating\tName\tAddress\tEmail\tCustomers\tBookings\tComplaints\tTotal Earning\n";
+        return header + String.join("\n", rows);
+    }
+
+    private static String formatSingleHotelRow(HotelStatistics hotel) {
+        return String.format(
+                "[%d]\t%.2f\t%s\t%s\t%s\t%d\t%d\t%d\t%.2f",
+                hotel.getHotelId(),
+                hotel.getAvgRating(),
+                hotel.getName(),
+                formatAddress(hotel),
+                hotel.getEmail(),
+                hotel.getNCustomers(),
+                hotel.getNBookings(),
+                hotel.getNComplaints(),
+                hotel.getTotalEarning()
         );
     }
 
