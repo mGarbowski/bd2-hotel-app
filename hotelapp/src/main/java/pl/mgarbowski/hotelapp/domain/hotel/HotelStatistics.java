96f3251e05943db958ba570a07ca5bfdd5e683a3
mgarbowski <mikolaj.garbowski@gmail.com>
2024-05-24 11:52:07 +0200

    add address data to hotel statistics view

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/hotel/HotelStatistics.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/hotel/HotelStatistics.java
@@ -11,6 +11,13 @@ import java.math.BigDecimal;
 public class HotelStatistics {
     @Id
     private Integer hotelId;
+    private String name;
+    private String email;
+    private BigDecimal avgRating;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String country;
     private Long nCustomers;
     private Integer nBookings;
     private Long nComplaints;
