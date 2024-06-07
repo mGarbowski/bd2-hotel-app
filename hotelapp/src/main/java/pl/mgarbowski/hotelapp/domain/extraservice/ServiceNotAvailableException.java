f9637721bd3176d38cb46afb37110d05a0420135
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:43:26 +0200

    Added JavaDoc documentation to Service errors

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/extraservice/ServiceNotAvailableException.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/extraservice/ServiceNotAvailableException.java
@@ -1,6 +1,14 @@
 package pl.mgarbowski.hotelapp.domain.extraservice;
 
+/**
+ * Exception thrown when a service is not available.
+ */
 public class ServiceNotAvailableException extends Exception {
+    /**
+     * Constructs a new ServiceNotAvailableException with the specified service name.
+     *
+     * @param serviceName the name of the unavailable service
+     */
     public ServiceNotAvailableException(String serviceName) {
         super(String.format("%s is not available for this booking", serviceName));
     }
