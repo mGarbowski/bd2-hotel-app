41ab9953539f11d46887f7d1f954e69127f9ba04
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:40:48 +0200

    Added JavaDoc documentation to HotelStatistics objects and related Repository

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/hotel/HotelStatisticsRepository.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/hotel/HotelStatisticsRepository.java
@@ -2,5 +2,9 @@ package pl.mgarbowski.hotelapp.domain.hotel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for {@link HotelStatistics} entities.
+ * Provides methods for performing CRUD operations on HotelStatistics entities.
+ */
 public interface HotelStatisticsRepository extends JpaRepository<HotelStatistics, Integer> {
 }
