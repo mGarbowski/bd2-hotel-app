85c1f2671f5f6110c8d3239b34413cd6d26ac224
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:38:02 +0200

    Added JavaDoc documentation to Complaint objects and related Repository and Services

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/booking/ComplaintService.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/booking/ComplaintService.java
@@ -5,13 +5,23 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 
+/**
+ * Service class for managing complaints.
+ */
 @Service
 @RequiredArgsConstructor
 public class ComplaintService {
     private final ComplaintRepository complaintRepository;
 
+    /**
+     * Creates a complaint for a given booking.
+     *
+     * @param booking       the booking associated with the complaint
+     * @param complaintText the text of the complaint
+     * @throws IllegalArgumentException if a complaint already exists for the booking
+     */
     public void createComplaint(Booking booking, String complaintText) throws IllegalArgumentException {
-        if(complaintRepository.getComplaintByBooking(booking).isPresent()) {
+        if (complaintRepository.getComplaintByBooking(booking).isPresent()) {
             throw new IllegalArgumentException("Complaint already exists for this booking");
         }
         var complaint = new Complaint();
