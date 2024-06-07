fc3c7aa5f91517d3f966b714313349a356944f8f
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:42:57 +0200

    Added JavaDoc documentation to PaymentSummary objects and related Repository

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/payment/PaymentSummaryRepository.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/payment/PaymentSummaryRepository.java
@@ -5,6 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Repository interface for {@link PaymentSummaryEntry} entities.
+ * Provides methods for performing CRUD operations on PaymentSummaryEntry entities.
+ */
 public interface PaymentSummaryRepository extends JpaRepository<PaymentSummaryEntry, UUID> {
+    /**
+     * Retrieves all payment summary entries for a given booking ID.
+     *
+     * @param bookingId the ID of the booking
+     * @return a list of payment summary entries for the booking
+     */
     List<PaymentSummaryEntry> findAllByBookingId(Integer bookingId);
 }
