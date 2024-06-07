2a7399876c045e3328942cdc7857feb73bb639ba
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:42:17 +0200

    Added JavaDoc documentation to Booking exceptions.

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/apartment/InvalidBookingDateException.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/apartment/InvalidBookingDateException.java
@@ -1,6 +1,14 @@
 package pl.mgarbowski.hotelapp.domain.apartment;
 
+/**
+ * Exception thrown when a booking date is invalid.
+ */
 public class InvalidBookingDateException extends Exception {
+    /**
+     * Constructs a new InvalidBookingDateException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public InvalidBookingDateException(String message) {
         super(message);
     }
