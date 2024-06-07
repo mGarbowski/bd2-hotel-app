d950b714452b3bca5a44450ccfa11346c85a702b
jedrzej-grabski <grabski.dev@gmail.com>
2024-06-07 19:35:07 +0200

    Added JavaDoc documentation to ApartmentStatistics objects its Repository.

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/apartment/ApartmentStatisticsRepository.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/apartment/ApartmentStatisticsRepository.java
@@ -2,5 +2,9 @@ package pl.mgarbowski.hotelapp.domain.apartment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for {@link ApartmentStatistics} entities.
+ * Provides methods for performing CRUD operations on ApartmentStatistics entities.
+ */
 public interface ApartmentStatisticsRepository extends JpaRepository<ApartmentStatistics, Integer> {
 }
