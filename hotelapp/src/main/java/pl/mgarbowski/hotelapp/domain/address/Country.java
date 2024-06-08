65dcc418ef175c870c111f21272a64110b432752
mgarbowski <mikolaj.garbowski@gmail.com>
2024-06-08 17:19:35 +0200

    add constructor to Country

--- hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/address/Country.java
+++ hotelapp/src/main/java/pl/mgarbowski/hotelapp/domain/address/Country.java
@@ -1,6 +1,8 @@
 package pl.mgarbowski.hotelapp.domain.address;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,7 +10,15 @@ import lombok.Data;
 @Data
 public class Country {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String code;
+
+    protected Country() {}
+
+    public Country(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
